ee0e6772f827d47d70d672be59763787e2aa618f
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-06-27 19:22:29 +0530

    Mini Random Number

--- project.py
+++ project.py
@@ -1,1 +1,15 @@
 import random
+
+target = random.randint(1, 100)
+
+while True:
+    userChoice = int(input("Guess the target : "))
+    if(userChoice == target):
+        print("Success : Correct Guess!! ")
+        break
+    if(userChoice < target):
+        print("Your number was to small. Take a bigger guess..")
+    else:
+        print("Your number was to big. Take a smaller guess..")
+
+print("-----GAME OVER-----")      
