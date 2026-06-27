161cbf656a6d989ed8d8f2e5c32c0f0769643783
Vaibhav Chauhan <vaibhavchauhan7299@gmail.com>
2026-06-27 19:55:19 +0530

    random password generator

---
+++ random password generator.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+#print(string.ascii_letters)
+#print(string.digits)
+#print(string.punctuation)
+
+#val = random.choice(['a', 'b', 'c', 'd',])
+#print(val)
+
+pass_len = 14
+charValues = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+for i in range(pass_len):
+    password += random.choice(charValues)
+
+print("your random password is:", password)
